3cf25c976e27d09c9482923c2164114f308f3705
Bertty Contreras-Rojas <berttycontreras@gmail.com>
2022-12-05 23:17:16 +0200

    [api][spark] seed implementation for HackitRDD
    Signed-off-by: Bertty Contreras-Rojas <berttycontreras@gmail.com>

---
+++ hackit-api/hackit-spark/src/main/java/de/tuberlin/dima/hackit/api/spark/rdd/HackitPairRDD.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2022 DIMA/TU-Berlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.tuberlin.dima.hackit.api.spark.rdd;
+
+import de.tuberlin.dima.hackit.core.tuple.HackitTuple;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function2;
+import scala.Tuple2;
+
+public class HackitPairRDD<K, KT, V>{
+  private JavaPairRDD<KT, HackitTuple<K, V>> rdd;
+
+  public HackitPairRDD(JavaPairRDD<KT, HackitTuple<K, V>> rdd) {
+    this.rdd = rdd;
+  }
+
+//  public HackitPairRDD<K, KT, V> reduceByKey(Function2<V, V, V> func) {
+//    return new HackitPairRDD<>(
+//        this.rdd
+//            .mapValues(
+//                HackitTuple::getValue
+//            )
+//            .reduceByKey(
+//                func
+//            )
+//            .mapValues(
+//                HackitTuple::new
+//            )
+//    );
+//  }
+//
+//  public HackitPairRDD<K, KT, V> join(HackitPairRDD<K, KT, V> other){
+//    JavaPairRDD<KT, Tuple2<HackitTuple<K, V>, HackitTuple<K, V>>> tmp = this.rdd.join(other.rdd);
+//    return null;
+//  }
+
+
+/*
+    /**
+     * Wrap the RDD inside of DebugRDD
+     * *
+    public static <T> DebugPairRDD<T> fromRDD(RDD<T> rdd){
+        return this(rdd, rdd.elementClassTag());
+    }
+    /**
+     * Wrap the JavaRDD inside of DebugRDD
+     * *
+    public static <T> DebugPairRDD<T> fromJavaRDD(JavaRDD<T> rdd){
+        return new DebugRDD<T>(rdd.rdd(), rdd.classTag());
+    }
+    /**
+     * unwrap the RDD and coming back to the normal execution
+     * *
+    public RDD<Tuple2<K, V>> toRDD(){
+        return this.toPairJavaRDD().rdd();
+    }
+    /**
+     * unwrap the RDD and coming back to the normal execution
+     * *
+    public JavaPairRDD<K, V> toPairJavaRDD(){
+        RDD<Tuple2<K, V>> tmp = this.rdd();
+        if(this.classTag().getClass() == HackItTuple.class) {
+            tmp = null;//((JavaRDD<HackItTuple<T>>)this.rdd).map(com.qcri.hackit -> com.qcri.hackit.getValue());
+        }
+        return super.fromRDD(tmp, this.kClassTag(), this.vClassTag());
+    }
+    public static <T> DebugRDD<T> wrapDebugRDD(JavaRDD<T> rdd){
+        return DebugRDD.fromJavaRDD(rdd);
+    }
+    @Override
+    public Map<K, V> reduceByKeyLocally(Function2<V, V, V> func) {
+        return super.reduceByKeyLocally(func);
+    }
+    @Override
+    public <W> JavaPairRDD<K, Tuple2<V, W>> join(JavaPairRDD<K, W> other) {
+        return super.join(other);
+    }
+    @Override
+    public void saveAsTextFile(String path) {
+        super.saveAsTextFile(path);
+    }*/
+}
---
+++ hackit-api/hackit-spark/src/main/java/de/tuberlin/dima/hackit/api/spark/rdd/HackitRDD.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2022 DIMA/TU-Berlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.tuberlin.dima.hackit.api.spark.rdd;
+
+import de.tuberlin.dima.hackit.core.tuple.HackitTuple;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.rdd.RDD;
+
+public class HackitRDD<K, T> {
+  private JavaRDD<HackitTuple<K, T>> rdd;
+
+  private HackitRDD(JavaRDD<HackitTuple<K, T>> rdd) {
+    this.rdd = rdd;
+  }
+
+  /**
+   * Wrap the JavaRDD inside DebugRDD
+   * */
+  public static <K, T> HackitRDD<K, T> fromJavaRDD(JavaRDD<T> rdd){
+    HackitRDD<K, T> ktHackItRDD = new HackitRDD<K, T>(
+        rdd.map((Function<T, HackitTuple<K, T>>) HackitTuple::new)
+    );
+    return ktHackItRDD;
+  }
+
+  /**
+   * unwrap the RDD and coming back to the normal execution
+   * */
+  public RDD<T> toRDD(){
+    return this.toJavaRDD().rdd();
+  }
+
+  /**
+   * unwrap the RDD and coming back to the normal execution
+   * */
+  public JavaRDD<T> toJavaRDD(){
+    return this.rdd.map(hackit -> hackit.getValue());
+  }
+
+  public static <K, T> HackitRDD<K, T> wrapDebugRDD(JavaRDD<T> rdd){
+    return HackitRDD.fromJavaRDD(rdd);
+  }
+  // Common RDD functions
+//
+//  public HackitRDD<K, T> filter(Function<T, Boolean> f) {
+//    HackitRDD<K, T> ktHackItRDD = new HackitRDD<>(
+//        this.rdd.filter(
+//            (Function<HackitTuple<K, T>, Boolean>) new PredicateHackItTagger<K, T>(f).addPostTag(new LogTag())
+//        )
+//    );
+//    return ktHackItRDD;
+//  }
+//
+//  public <O> HackitRDD<K, O> map(Function<T, O> f){
+//    HackitRDD<K, O> ktHackItRDD = new HackitRDD<>(
+//        this.rdd.map(
+//            new FunctionHackItTagger<>(f)
+//        )
+//    );
+//    return ktHackItRDD;
+//  }
+//
+//  public <KO, O> HackitRDD<K, KO, O> mapToPair(
+//      PairFunction<T, KO, O> pairFunction){
+//    HackitRDD<K, KO, O> ktDebugRDD = new HackitRDD<>(
+//        this.rdd.mapToPair(
+//            new PairFunctionHackItTagger<>(pairFunction)
+//        )
+//    );
+//    return ktDebugRDD;
+//  }
+//
+//  public <KO, O> HackitRDD<K, O> flatMap(FlatMapFunction<T, O> flatMapFunction){
+//    HackitRDD<K, O> koHackItRDD = new HackitRDD<>(
+//        this.rdd.flatMap(
+//            new FlatMapFunctionHackItTagger<K, T, O>(
+//                flatMapFunction
+//            )
+//        )
+//    );
+//    return koHackItRDD;
+//  }
+//
+//  public HackitRDD<K, T> sniffer(){
+//    HackItSnifferSpark<Long, String, byte[], SenderMultiChannelRabbitMQ<byte[]>, ReceiverMultiChannelRabbitMQ<Long, String>> lala = new HackItSnifferSpark<Long, String, byte[], SenderMultiChannelRabbitMQ<byte[]>, ReceiverMultiChannelRabbitMQ<Long, String>>(
+//        new EmptyHackItInjector<>(),
+//        ele -> true,
+//        new HachitShipperDirectRabbitMQ(),
+//        ele -> true,
+//        new DefaultCloner<HackitTuple<Long, String>>()
+//    );
+//    return this.sniffer(lala);
+//  }
+//
+//  public HackitRDD<K, T> sniffer(HackItSnifferSpark hackItSniffer){
+//    return new HackitRDD<>(
+//        this.rdd.flatMap(
+//            hackItSniffer
+//        )
+//    );
+//  }
+
+    /*@Override
+    public <R> DebugRDD<R> map(Function<T, R> f) {
+        return null;
+    }
+    @Override
+    public <K2, V2> DebugRDD<K2, V2> mapToPair(PairFunction<T, K2, V2> f) {
+        return null;
+    }
+    @Override
+    public <U> DebugRDD<U> flatMap(FlatMapFunction<T, U> f) {
+        return null;
+    }
+    @Override
+    public <K2, V2> DebugRDD<K2, V2> flatMapToPair(PairFlatMapFunction<T, K2, V2> f) {
+        return null;
+    }
+    */
+
+  public void saveAsTextFile(String path) {
+    this.toJavaRDD().saveAsTextFile(path);
+  }
+}
