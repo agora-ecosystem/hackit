01b17fce6595f979f7bd87aea262e6e52c6d13ed
Bertty Contreras-Rojas <berttycontreras@gmail.com>
2022-12-05 15:50:58 +0200

    [shipper][kafka] bring the code from the wayang code from the commit b4f375341470b85d398a3051b096fc2db5f2a6bd
    Signed-off-by: Bertty Contreras-Rojas <berttycontreras@gmail.com>

---
+++ hackit-shipper/hachit-shipper-kafka/src/main/java/de/tuberlin/dima/hackit/shipper/kafka/receiver/ReceiverKafka.java
@@ -0,0 +1,94 @@
+package de.tuberlin.dima.hackit.shipper.kafka.receiver;
+
+import de.tuberlin.dima.hackit.core.sniffer.shipper.PSProtocol;
+import de.tuberlin.dima.hackit.core.sniffer.shipper.receiver.Receiver;
+import de.tuberlin.dima.hackit.core.tuple.HackitTuple;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class ReceiverKafka<K, T> extends Receiver<HackitTuple<K, T>> implements
+    PSProtocol {
+
+    //TODO Get from configuration
+    static Map<String, String> KAFKA_MAPPING;
+    static {
+        KAFKA_MAPPING = new HashMap<>();
+        KAFKA_MAPPING.put("127.0.0.1", "127.0.0.1");
+    }
+    static Integer numPartitions = 1;
+    static Short replicationFactor = 1;
+
+
+    Consumer<K, T> consumer;
+    Properties config;
+    List<String> topics;
+
+    public ReceiverKafka(Properties config){
+        this.config = config;
+        this.topics = new ArrayList<>();
+    }
+
+
+    @Override
+    public PSProtocol addTopic(String... topic) {
+
+        this.topics.addAll(Arrays.asList(topic));
+        this.consumer.subscribe(this.topics);
+        return this;
+    }
+
+    @Override
+    public PSProtocol addExchange(String exchange) {
+        return null;
+    }
+
+    @Override
+    public void init() {
+        this.consumer =
+                new KafkaConsumer<>(config);
+    }
+
+    @Override
+    public Iterator<HackitTuple<K, T>> getElements() {
+
+        final int giveUp = 100;   int noRecordsCount = 0;
+        ConsumerRecords<K, T> consumerRecords;
+        while (true) {
+            consumerRecords =
+                    consumer.poll(1000);
+
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) break;
+                else continue;
+            }
+
+            List<HackitTuple<K, T>> list = new ArrayList<>();
+            consumerRecords.forEach(record ->{
+                HackitTuple<K, T> result = new HackitTuple<>(
+                        record.value()
+                );
+                // System.out.println("received " + record.value());
+                list.add(result);
+            });
+
+            consumer.commitAsync();
+
+            return list.listIterator();
+        }
+        return null;
+    }
+
+    @Override
+    public void close() {
+        consumer.close();
+    }
+}
